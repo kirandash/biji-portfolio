ac52e28805d8836d10cbc286304f6351ab281fd5
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-04 08:27:14 +0530

    Build out wordpress navigation

--- wp-content/themes/biji-portfolio/header.php
+++ wp-content/themes/biji-portfolio/header.php
@@ -8,13 +8,23 @@
     <?php wp_head(); ?>
   </head>
 
-  <body>
+  <body <?php body_class(); ?>>
     <header class="row no-max pad main">
   <h1><a class='current' href="<?php bloginfo('url');?>"><?php bloginfo('sitename');?></a></h1>
   <a href="" class="nav-toggle"><span></span>Menu</a>
   <nav>
     <h1 class="open"><a class='current' href="<?php bloginfo('url');?>"><?php bloginfo('sitename');?></a></h1>
-    <ul class="no-bullet">
+    
+    <?php
+		$defaults = array(
+			'container' => false,
+			'theme_location' => 'primary-menu',
+			'menu_class' => 'no-bullet'
+		);
+		
+		wp_nav_menu($defaults);
+	?>
+<!--    <ul class="no-bullet">
       <li class="current parent"><a class='current' href="<?php bloginfo('url');?>">Portfolio</a>
         <ul class="sub-menu">
           <li><a href="item.html">Portfolio Item</a></li>
@@ -32,5 +42,6 @@
       <li><a href="about.html">About</a></li>
       <li><a href="contact.html">Contact</a></li>
     </ul>
+-->
   </nav>
 </header>
